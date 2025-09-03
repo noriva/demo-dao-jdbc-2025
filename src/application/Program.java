7bb6ef6a77df55114b939cb9abb9c256a184e6d1
Norival Marson Junior <jrmarson.nmj@gmail.com>
2025-09-03 16:11:52 -0300

    DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Departament;
 import model.entities.Seller;
 
@@ -10,6 +12,7 @@ public class Program {
 	public static void main(String[] args) {
 		Departament obj = new Departament(1, "Books");
 		Seller seller = new Seller(1, "Bob", "bog@gmail", new Date(), 3.000, obj);  
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		System.out.println(seller);
 	}
 
